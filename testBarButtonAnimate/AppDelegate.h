487326f1f4c18acf30940403e4dc8d104dd1ddc9
Piao Piao <94313353@qq.com>
2017-04-11 15:53:46 +0800

    Initial Commit

---
+++ testBarButtonAnimate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testBarButtonAnimate
+//
+//  Created by Piao Piao on 2017/4/11.
+//  Copyright © 2017年 Piao Piao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testBarButtonAnimate/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testBarButtonAnimate
+//
+//  Created by Piao Piao on 2017/4/11.
+//  Copyright © 2017年 Piao Piao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
