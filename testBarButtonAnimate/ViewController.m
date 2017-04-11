f3c1c9839d52d1f2aca01df7000017cc606651b2
Piao Piao <94313353@qq.com>
2017-04-11 16:05:28 +0800

    first commit

--- testBarButtonAnimate/ViewController.m
+++ testBarButtonAnimate/ViewController.m
@@ -8,8 +8,8 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
-
+@interface ViewController ()<UITableViewDelegate,UITableViewDataSource>
+@property(nonatomic,strong) UITableView* tableView;
 @end
 
 @implementation ViewController
@@ -17,6 +17,12 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+//    self.navigationItem.l
+    self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"test" style:UIBarButtonItemStyleDone target:self action:@selector(testAnimate)];
+    self.tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, 0) style:UITableViewStylePlain];
+    self.tableView.dataSource = self;
+    self.tableView.delegate = self;
+    [self.view addSubview:self.tableView];
 }
 
 
@@ -25,5 +31,36 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
 
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 10;
+}
+
+- (UITableViewCell*)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell* cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:nil];
+    cell.textLabel.text = [indexPath description];
+    cell.contentView.backgroundColor = [UIColor darkGrayColor];
+    cell.backgroundColor = [UIColor darkGrayColor];
+    return cell;
+}
+
+- (void)testAnimate
+{
+    static BOOL isExpand = NO;
+    CGFloat desireHeight = 0;
+    if (!isExpand)
+    {
+        desireHeight = 10*44;
+    }
+    isExpand = !isExpand;
+    [UIView animateWithDuration:0.3 animations:^{
+        self.tableView.frame = CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, desireHeight);
+    }];
+}
 @end
